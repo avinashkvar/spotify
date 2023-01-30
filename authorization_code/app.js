56ecff6d0396702527470b70bbdf3a0b7b9e7902
avinashkvar <k3076183@gmail.com>
2023-01-30 14:31:27 +0530

    new commit

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -109,40 +109,48 @@ app.get('/spotify-login', function (req, res) {
 			json: true,
 		};
 
-		request.post(authOptions, function (error, response, body) {
-			if (!error && response.statusCode === 200) {
-				var access_token = body.access_token,
-					refresh_token = body.refresh_token;
+		request.post(
+			authOptions,
+			function (error, response, body) {
+				if (!error && response.statusCode === 200) {
+					var access_token = body.access_token,
+						refresh_token = body.refresh_token;
 
-				// var options = {
-				// 	url: 'https://api.spotify.com/v1/me',
-				// 	headers: { Authorization: 'Bearer ' + access_token },
-				// 	json: true,
-				// };
+					// var options = {
+					// 	url: 'https://api.spotify.com/v1/me',
+					// 	headers: { Authorization: 'Bearer ' + access_token },
+					// 	json: true,
+					// };
 
-				// //use the access token to access the Spotify Web API
-				// request.get(options, function (error, response, body) {
-				// 	console.log(body);
-				// 	res.send({ token: access_token, body: body });
-				// });
-				res.send({ token: access_token ,refresh_token:refresh_token});
-				// we can also pass the token to the browser to make requests from there
-				// res.redirect(
-				// 	'/#' +
-				// 		querystring.stringify({
-				// 			access_token: access_token,
-				// 			refresh_token: refresh_token,
-				// 		}),
-				// );
-			} else {
-				res.redirect(
-					'/#' +
-						querystring.stringify({
-							error: 'invalid_token',
-						}),
-				);
-			}
-		});
+					// //use the access token to access the Spotify Web API
+					// request.get(options, function (error, response, body) {
+					// 	console.log(body);
+					// 	res.send({ token: access_token, body: body });
+					// });
+					res.send({ token: access_token, refresh_token: refresh_token });
+					// we can also pass the token to the browser to make requests from there
+					// res.redirect(
+					// 	'/#' +
+					// 		querystring.stringify({
+					// 			access_token: access_token,
+					// 			refresh_token: refresh_token,
+					// 		}),
+					// );
+				} else {
+					res.redirect(
+						'/#' +
+							querystring.stringify({
+								error: 'invalid_token',
+							}),
+					);
+				}
+			},
+			{
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+				},
+			},
+		);
 	}
 });
 
