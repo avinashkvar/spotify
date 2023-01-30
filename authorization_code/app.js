a36ee0f5f84f15304611b36e4cc8a404c9b61dfa
avinashkvar <k3076183@gmail.com>
2023-01-30 14:11:37 +0530

    new commit

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -39,7 +39,16 @@ var stateKey = 'spotify_auth_state';
 var app = express();
 
 app.use(cors()).use(cookieParser());
-
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'Content-Type',
+		'Authorization',
+	);
+	next();
+});
 app.get('/', (req, res) => {
 	res.send({ data: 'hello' });
 });
